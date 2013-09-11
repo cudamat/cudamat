a4f1f837689dfb8eb32b221ea5d7bb32a2532196
f0k <github@jan-schlueter.de>
2013-09-11 18:49:59 +0200

    Fix warning about double/float conversion in kReciprocal() kernel

--- cudamat_kernels.cu
+++ cudamat_kernels.cu
@@ -584,7 +584,7 @@ __global__ void kReciprocal(float* mat, float* target, unsigned int len) {
     const unsigned int numThreads = blockDim.x * gridDim.x;
 
     for (unsigned int i = idx; i < len; i += numThreads)
-        target[i] = 1. / mat[i];
+        target[i] = 1.f / mat[i];
 }
 
 __global__ void kAddColVector(float* mat, float* vec, float* tgtMat, unsigned int width,
