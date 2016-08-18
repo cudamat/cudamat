931ef9234e521a222206be2dcb889f8459f9c164
AminHP <mdan.hagh@gmail.com>
2016-08-18 18:26:02 +0430

    Add correlate() function

--- cudamat/cudamat_kernels.cu
+++ cudamat/cudamat_kernels.cu
@@ -781,3 +781,24 @@ __global__ void kWhere(float* condition_mat, float* if_mat, float* else_mat, flo
         target[i] = condition_mat[i] ? if_mat[i] : else_mat[i];
     }
 }
+
+
+__global__ void kCorrelate(float* source, float* kernel, float* dest, int width, int height, int kwidth, int kheight) {
+    const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int numThreads = blockDim.x * gridDim.x;
+
+    for (unsigned int i = idx; i < width * height; i += numThreads) {
+        float sum = 0;
+        for (int w = -kwidth/2; w <= kwidth/2; w++) {
+            for (int h = -kheight/2; h <= (kheight)/2; h++) {
+                const int x = (i / height) + w;
+                const int y = (i % height) + h;
+                const int j = i + (w * height) + h;
+
+                if (x >= 0 && x < width && y >= 0 && y < height)
+                    sum += source[j] * kernel[(kwidth * kheight / 2) + w * kheight + h];
+            }
+        }
+        dest[i] = sum;
+    }
+}
